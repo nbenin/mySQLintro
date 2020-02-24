4cc2a04806c0dcf5296cf9a2df160f8f5e4423b1
Neil <neil.mf.benin@gmail.com>
2020-02-24 14:51:53 +0100

    got the PDOfile

---
+++ connection.php
@@ -0,0 +1,14 @@
+<?php
+function openConnection() {
+// Try to figure out what these should be for you
+$dbhost    = "localhost";
+$dbuser    = "neil";
+$dbpass    = "neil";
+$db        = "becode";
+
+// Try to understand what happens here
+$pdo = new PDO('mysql:host='. $dbhost . ';dbname='. $db, $dbuser, $dbpass);
+var_dump($pdo);
+// Why we do this here
+return $pdo;
+}
