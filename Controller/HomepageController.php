f0ab3fcdeaca6652cc96643ec9823c9f56395736
Neil <neil.mf.benin@gmail.com>
2020-02-25 11:00:16 +0100

    yaaaas

--- Controller/HomepageController.php
+++ Controller/HomepageController.php
@@ -1,12 +1,9 @@
 <?php
+declare(strict_types=1);
+
 class HomepageController {
     public function render($post) {
 
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $userInfo = new Post($post['firstName'], $post['lastName'], $post['username'], $post['linkedin'],
-                                $post['github'], $post['email'], $post['language'], $post['avatar'],
-                                $post['video'], $post['quote'], $post['quoteAuthor']);
-        }
         // Connect to database
         $pdo = openConnection();
         if ($pdo) {
@@ -15,6 +12,14 @@ class HomepageController {
             echo 'Connection Problem';
         }
 
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $statementGenerator = new StatementGenerator();
+            $userInfo = new Post($post['firstName'], $post['lastName'], $post['username'], $post['linkedin'],
+                                $post['github'], $post['email'], $post['language'], $post['avatar'],
+                                $post['video'], $post['quote'], $post['quoteAuthor']);
+
+            $sqlInsert = $statementGenerator->generateInsert($userInfo);
+        }
 
 
         $statement = $pdo->prepare("SELECT * FROM student")->execute($statement);
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 require 'Controller/HomepageController.php';
+require 'Model/StatementGenerator.php';
 require 'Model/connection.php';
 
 $controller = new HomepageController();
